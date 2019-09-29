6169c66554f352a1ba3d34c349349ea4ad6dd383
leeyoungseung <leeyoungseung@DESKTOP-QG5IT2R>
2019-09-29 17:51:41 +0900

    jsonp api using spring boot

---
+++ src/main/java/com/example/jsonp_api/JsonpApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.jsonp_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonpApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jsonp_api/JsonpApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.jsonp_api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JsonpApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
